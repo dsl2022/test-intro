3485748a0c74d8dd0ea66072ad8b40f1cabc6d2f
dsl2022 <2015rpro@gmail.com>
2023-05-11 22:27:54 -0400

    first commit

---
+++ demo.js
@@ -0,0 +1,42 @@
+const calculator = {
+    add:(a,b)=>{
+        return a+b
+    },
+    sub:(a,b)=>{
+        return a-b
+    },
+    mul:(a,b)=>{
+        return a*b
+    },
+    div:(a,b)=>{
+        return a/b
+    },
+}
+// Addition:
+// Test adding two positive numbers.
+// Test adding two negative numbers.
+// Test adding a positive number and zero.
+// Test adding a negative number and zero.
+// Test adding a positive number and a negative number.
+
+// Subtraction:
+// Test subtracting a smaller positive number from a larger positive number.
+// Test subtracting a larger positive number from a smaller positive number.
+// Test subtracting a positive number from zero.
+// Test subtracting zero from a positive number.
+// Test subtracting a negative number from a positive number.
+
+// Multiplication:
+// Test multiplying two positive numbers.
+// Test multiplying two negative numbers.
+// Test multiplying a positive number by zero.
+// Test multiplying zero by a positive number.
+// Test multiplying a positive number by a negative number.
+
+// Division:
+// Test dividing a positive number by another positive number.
+// Test dividing a negative number by another negative number.
+// Test dividing a positive number by one.
+// Test dividing zero by a positive number.
+// Test dividing a positive number by zero (should result in an error).
+// These scenarios cover different arithmetic operations with various combinations of positive, negative, and zero values. By testing these cases, you can verify the accuracy of the calculator's calculations.
